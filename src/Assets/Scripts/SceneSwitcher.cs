722c659a5da3ea9b03fe47097c9bb54442383258
Andris Grishan <andris.grishan@gmail.com>
2020-02-02 07:41:42 +0200

    scene manager

---
+++ src/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public static SceneSwitcher instance = null;
+
+    private int _currentSceneIndex = 0;
+
+    public string[] Scenes = new[]
+    {
+        "lvl1",
+        "lvl2"
+    };
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
